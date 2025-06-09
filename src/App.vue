6cfd6677909788747b3aa547d02f10d08e434a52
dmytkach <dmytro.tkach@teamdev.com>
2025-06-09 20:51:51 +0300

    delete all files

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,19 @@
   <div>
     <HeaderComponent title="Hello title properties!" age="28" city="Odesa"/>
   </div>
+  <HelloWorld msg = "Тут буде заголовок" />
 </template>
 
 <script>
 import HeaderComponent from "./components/Header.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
+    HelloWorld
   }
 }
 </script>
