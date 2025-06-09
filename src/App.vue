f4c381835ce6693a61793813ea8dd6e397870d72
dmytkach <dmytro.tkach@teamdev.com>
2025-06-09 20:17:44 +0300

    improve file structures

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,15 @@
   <div>
     <HeaderComponent title="Hello title properties!" age="28" city="Odesa"/>
   </div>
-  <MessageComponent title="Привіт, Vue!" message="Я вмію створювати компоненти із пропсами!" />
 </template>
 
 <script>
 import HeaderComponent from "./components/Header.vue";
-import MessageComponent from "./components/MessageComponent.vue";
 
 export default {
   name: 'App',
   components: {
     HeaderComponent,
-    MessageComponent
   }
 }
 </script>
